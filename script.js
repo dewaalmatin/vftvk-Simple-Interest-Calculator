89bd005c0f769d27bab761d4aab8d49975a45be0
dewaalmatin <74178112+dewaalmatin@users.noreply.github.com>
2022-08-21 02:27:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    principal = document.getElementById("principal")
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value
     deposit = document.getElementById("deposit");
@@ -9,10 +10,17 @@ function compute()
     date = document.getElementById("date");
     result = document.getElementById("result");
 
+    if(0 >= p) {
+        alert(
+            "Enter a positive number"
+        );
+        principal.focus();
+        return;
+    };
+
     result.style.display = "block";
     deposit.innerHTML = p;
     interest.innerHTML = r;
     amount.innerHTML = ((p * r)/100)*y;
     date.innerHTML = 2020 + parseInt(y)
 }
-        
