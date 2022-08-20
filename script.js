a836b1ca76740e5b4b7111cbe679742525b98093
dewaalmatin <74178112+dewaalmatin@users.noreply.github.com>
2022-08-20 19:58:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value
+    deposit = document.getElementById("deposit");
+    interest = document.getElementById("interest");
+    amount = document.getElementById("amount");
+    date = document.getElementById("date");
+    result = document.getElementById("result");
+
+    result.style.display = "block";
+    deposit.innerHTML = p;
+    interest.innerHTML = r;
+    amount.innerHTML = ((p * r)/100)*y;
+    date.innerHTML = 2020 + parseInt(y)
 }
         
